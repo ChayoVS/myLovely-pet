6866001d540049ff0da8e2a0792f506799215ce2
rosario VS <paradisekiss-yaoi@hotmail.com>
2020-02-02 17:05:50 -0600

    Updates

---
+++ precache-manifest.6a099f27eeafefaae17661843aa819be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049980da1133e7be92092ab46ee62caf",
+    "url": "/myLovely-pet/index.html"
+  },
+  {
+    "revision": "fad882d6dc0235147a68",
+    "url": "/myLovely-pet/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ed088fc3bc8c9679c7df",
+    "url": "/myLovely-pet/static/js/2.2bc1e90c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myLovely-pet/static/js/2.2bc1e90c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad882d6dc0235147a68",
+    "url": "/myLovely-pet/static/js/main.a59c0d94.chunk.js"
+  },
+  {
+    "revision": "610c52ea5d33cd7932cf",
+    "url": "/myLovely-pet/static/js/runtime-main.6f87c652.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myLovely-pet/static/media/logo.5d5d9eef.svg"
+  }
+]);
